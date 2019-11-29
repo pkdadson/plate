1fee11eaa9aae15e083d13662e0de621d84f0805
Dadson Papa Dadze <pddadson@gmail.com>
2019-11-29 11:56:43 +0000

    first commit

--- src/main.js
+++ src/main.js
@@ -3,11 +3,27 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import VueSnotify from 'vue-snotify';
+import 'bootstrap/dist/css/bootstrap.css'
+import ProgressBar from 'vue-progressbar'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
+library.add(faUserSecret)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.use(ProgressBar, {
+    color: 'rgb(143, 255, 199)',
+    failedColor: 'red',
+    height: '2px'
+})
+Vue.use(VueSnotify, { toast: { showProgressBar: false } })
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')

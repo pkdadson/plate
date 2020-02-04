bf841788a25b50942256c676e3e17a398df0de4a
pkdadson <38428845+pkdadson@users.noreply.github.com>
2020-02-04 22:47:36 +0000

    About.vue

--- src/views/Home/About.vue
+++ src/views/Home/About.vue
@@ -2,20 +2,7 @@
   <div class="about">
     <h1>This is an about page</h1>
     <p>
-      ABOUT PRIZE
-The Tekpreneur Prize provides aspiring student entrepreneurs at the Kwame
-Nkrumah University of Science and Technology an opportunity to present their
-business ideas and compete for cash prizes. The competition is preceded by
-events such as screening of ideas and a start-up school. The competition is open
-to current KNUST students at any level of study. Students will be given the
-opportunity to form teams of 1-3 member(s) and register their business ideas
-under any of these areas business: Media, Agriculture, Science and Technology,
-Energy, Real Estate, Sanitation, Sports, Health, Trade, Transportation, Creative
-arts and entertainment, banking and finance. All business ideas must have an
-integration of digitization. Every idea must include an integration of digitization.
-The inclusion of digitization purports to make start-ups globally competitive as
-many firms have thrived with digitization as a backbone of their respective
-businesses. 
+    ABOUT STREEET INC.
     </p>
   </div>
 </template>
